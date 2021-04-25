f3354972601962feb78b2b259120ed1b173a5886
koltennay52 <60151320+koltennay52@users.noreply.github.com>
2021-04-25 00:33:00 -0600

    updated Riot API key for 24 hours

--- src/javascripts/config/vars.js
+++ src/javascripts/config/vars.js
@@ -1,3 +1,3 @@
 export const AUTH_TOKEN_EXPIRES_IN = 7;
 export const APP_SECRET = "changeforproduction";
-export const riotKey = "RGAPI-50e0f588-f91c-4300-9f6e-6cb5be22ffa6";
+export const riotKey = "RGAPI-7724ea3e-cdcc-4c1d-b06b-1b4ce9436fae";
